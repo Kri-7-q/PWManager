5e37696822a46161e3a63e3e507be5268df5366f
Christian Weiß <weiss-christian@hotmail.de>
2016-04-22 22:18:00 +0200

    Decorator class for comparison.
    Docorator to compare two QHash objects (Account or OptionTable).
    Some fixes in XmlPersistence.
    Setter for print order of attributes in ConsoleInterface.

---
+++ Decorator/optiontblcomparedecorator.cpp
@@ -0,0 +1,7 @@
+#include "optiontblcomparedecorator.h"
+
+OptionTblCompareDecorator::OptionTblCompareDecorator()
+{
+
+}
+
---
+++ Decorator/optiontblcomparedecorator.h
@@ -0,0 +1,11 @@
+#ifndef OPTIONTBLCOMPAREDECORATOR_H
+#define OPTIONTBLCOMPAREDECORATOR_H
+
+
+class OptionTblCompareDecorator
+{
+public:
+    OptionTblCompareDecorator();
+};
+
+#endif // OPTIONTBLCOMPAREDECORATOR_H
